b843e9a1681756565ad38db83465e388b98872e2
ernestoc14 <descreed34@hotmail.com>
2025-06-28 13:10:00 -0500

    Create a new portfolio in Next.js

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/app/components/Hero/Hero";
+import Footer from "@/app/components/Footer/Footer"
+
+export default function Home() {
+    return (
+        <>
+            <header>
+                <Hero />
+            </header>
+            <main>
+                {/* Sections adn Components as Abou - Works- Projects and More */}
+                
+                <Footer />
+            </main>
+        </>
+    );
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// This page will have a slugs for each project
+// gotta read about dynamic routes in Next.js Docs
+const Work = () => {
+    return (
+        <div>Work</div>
+    )
+}
+
+export default Work
