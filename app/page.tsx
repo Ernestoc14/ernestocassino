0af699febe4f5a85af409068e23009e840b91ea7
ernestoc14 <ecassinoc@copaair.com>
2025-07-26 23:43:40 -0500

    Adding a Button Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/sections/Hero";
 import Footer from "@/sections/Footer"
+import Button from "@/components/Button";
 
 export default function Home() {
     return (
@@ -8,8 +9,10 @@ export default function Home() {
                 <Hero />
             </header>
             <main>
+                <Button />
+                <Button />
                 {/* Sections adn Components as Abou - Works- Projects and More */}
-                
+                {/* NavBar and redirect to other pages */}
                 <Footer />
             </main>
         </>
---
+++ components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <button className='bg-blue-800 rounded '>
+            Click Here!
+        </button>
+    )
+}
+
+export default Button
